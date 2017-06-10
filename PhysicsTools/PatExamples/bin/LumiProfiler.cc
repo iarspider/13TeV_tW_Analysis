3b979e667865074b2e0aa432bef51c28682b306a
Ivan Razumov <ivan.razumov@cern.ch>
2017-06-10 10:17:39 +0200

    Added tool to get lumi distribution

---
+++ PhysicsTools/PatExamples/bin/LumiProfiler.cc
@@ -0,0 +1,30 @@
+//
+// Created by razumov on 6/10/17.
+//
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TFileCollection.h>
+#include <TH1D.h>
+
+int main(int, char **) {
+    TFile f("MyMCTruePileupHistogram", "RECREATE");
+    f.cd();
+    TChain ch("IIHEAnalysis");
+    TFileCollection fc("dum", "", "xrd.list");
+    ch.AddFileInfoList(fc.GetList());
+    Int_t mc_trueNumInteractions;
+    ch.SetBranchStatus("*", 0);
+    ch.SetBranchStatus("mc_trueNumInteractions", 1);
+    ch.SetBranchAddress("mc_trueNumInteractions", &mc_trueNumInteractions);
+    TH1D hPileupMC("h", "h", 750, 0, 75);
+
+    for (UInt_t i = 0; i < ch.GetEntries(); i++)
+    {
+        ch.GetEntry(i);
+        hPileupMC.Fill(mc_trueNumInteractions);
+    }
+
+    hPileupMC.Write();
+    f.Close();
+}
